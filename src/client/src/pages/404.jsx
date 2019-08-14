fcd37b5d38f40e7397e0d93a70815eeb546642d4
Matthew Daniel Murphy <matt@mattmurphy.ca>
2019-08-14 02:12:42 -0400

    remove headingBackgroundImage from 404 page (no prop passed in anyway)

--- src/client/src/pages/404.jsx
+++ src/client/src/pages/404.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Page from '../components/jsx/Page'
 
 export default () => (
-	<Page headingBackgroundImage={this.props.headingBackgroundImage} id="404" heading="404: Page not found">
+	<Page id="404" heading="404: Page not found">
 		<section className="text">
 			<h2>A page does not exist at this location. Sorry about that!</h2>
 		</section>
