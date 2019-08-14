ddc4adae9e078fa7367a8ab342a8cb48c9a6f8f4
Matthew Daniel Murphy <matt@mattmurphy.ca>
2019-08-14 02:22:01 -0400

    fetchPostByName gives 'not found' response if post not found

--- src/routes/posts/fetchPostByName.js
+++ src/routes/posts/fetchPostByName.js
@@ -2,7 +2,8 @@ const Post = require('../../models/Post')
 
 module.exports = async (req, res) => {
 	const name = req.params.name
-	Post.findOne({ title: name.split('-').join(' ') }).then((post) => {
-		res.json(post)
+	Post.findOne({ title: name.split('-').join(' ') }).then(post => {
+		if (post) res.json({ post })
+		else res.json('not found')
 	})
 }
